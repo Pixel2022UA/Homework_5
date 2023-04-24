90f1ebf17b7867b5064dd69b7b56d07bd33a5d66
Pixel2022UA <zelenskiy2013@gmail.com>
2023-04-24 21:12:09 +0300

    fix all files

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("generate-teachers/", include("teachers.urls")),
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
 ]
